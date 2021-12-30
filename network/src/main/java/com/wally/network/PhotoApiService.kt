1bb1239f7de0d691a0fc3cb67e7f31e5464f2887
sikbongda <jys1016@gmail.com>
2021-12-30 23:07:19 +0900

    Ktor 엔진 교체 - CIO -> Android engine - log level = BODY

--- network/src/main/java/com/wally/network/PhotoApiService.kt
+++ network/src/main/java/com/wally/network/PhotoApiService.kt
@@ -3,7 +3,8 @@ package com.wally.network
 import android.util.Log
 import com.wally.network.response.Photo
 import io.ktor.client.*
-import io.ktor.client.engine.cio.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.logging.*
 
@@ -15,13 +16,19 @@ interface PhotoApiService {
 
         fun create(): PhotoApiService {
             return PhotoApiServiceImpl(
-                client = HttpClient(CIO) {
+                client = HttpClient(Android) {
                     install(Logging) {
+                        level = LogLevel.BODY
                         logger = object : Logger {
                             override fun log(message: String) {
                                 Log.d(TAG, message)
                             }
                         }
+                    }
+                    install(HttpTimeout) {
+                        requestTimeoutMillis = 15000L
+                        connectTimeoutMillis = 15000L
+                        socketTimeoutMillis = 15000L
                     }
                     install(JsonFeature) {
                         serializer = GsonSerializer {
